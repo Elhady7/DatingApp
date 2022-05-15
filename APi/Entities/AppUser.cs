ad5a80c38801616f32dda4582c0a2dcb99a8de9f
Elhady7 <Hadysalah632@gmail.com>
2022-05-16 01:46:01 +0200

    Updating AppUser To Get Password hash and salt

--- APi/Entities/AppUser.cs
+++ APi/Entities/AppUser.cs
@@ -3,6 +3,8 @@ namespace APi.Entities
     public class AppUser
     {
         public int Id { get; set; }
-        public string  UserName { get; set; }         
+        public string  UserName { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] SaltHash { get; set; }
     }
 }
