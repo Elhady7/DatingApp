359526ee6f7089bf03795052a39f6d33ccc9e39c
Elhady7 <Hadysalah632@gmail.com>
2022-05-23 08:19:49 +0200

    adding Token Config and services

---
+++ APi/Interfaces/ITokenService.cs
@@ -0,0 +1,9 @@
+using APi.Entities;
+
+namespace APi.Interfaces
+{
+    public interface ITokenService
+    {
+         string CreateToken(AppUser appUser);
+    }
+}
--- APi/Startup.cs
+++ APi/Startup.cs
@@ -1,8 +1,12 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using APi.Data;
+using APi.Interfaces;
+using APi.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -12,6 +16,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 
 namespace APi
@@ -29,6 +34,7 @@ namespace APi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<ITokenService,TokenService>();
             services.AddDbContext<DataContext>(options =>
             {
                 options.UseSqlServer(_config.GetConnectionString("DefaultConnection"));
@@ -39,6 +45,17 @@ namespace APi
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "APi", Version = "v1" });
             });
             services.AddCors();
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options=>
+            {
+                options.TokenValidationParameters =new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey=true,
+                    IssuerSigningKey=new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["TokenKey"])),
+                    ValidateIssuer=false,
+                    ValidateAudience=false
+                };
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -55,6 +72,8 @@ namespace APi
 
             app.UseRouting();
             app.UseCors(policy =>policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+            
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
